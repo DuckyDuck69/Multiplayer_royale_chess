f4b727ba288bf8e45cd16ead02a294ee5e73edcb
DuckyDuck69 <phanthaihoangduc99@gmail.com>
2025-02-12 20:11:28 -0500

    add obstacle visual to the board

--- client/index.js
+++ client/index.js
@@ -51,15 +51,25 @@ ctx.scale(devicePixelRatio, devicePixelRatio); //scale according to the API valu
 let grid = [];
 let columns = 16;
 let rows = 16;
+ //imagine clockwised direction where up is on top of your head, then right, then down, and then left
+const directions = [
+    0,  //up
+    1,  //right
+    2,  //down
+    3   //left
+];
 
 let size = (displayHeight / columns);  //calculate the size of each square
 
 //create a Cell object, which can be assigned method for later use
-function Cell(x, y){                        
-    this.x = x;
-    this.y = y;
-    this.show = function(color){              //declare a method of the Cell object, which is used to color Cell
-        let x_coor = this.x * size;           
+class Cell {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.isObstacle = false;
+    }
+    show(color) {
+        let x_coor = this.x * size;
         let y_coor = this.y * size;
         ctx.fillStyle = color;
         ctx.fillRect(x_coor, y_coor, size, size);
@@ -69,6 +79,7 @@ function Cell(x, y){
 window.addEventListener('load', function() {   //draw the board after the html/css load
     createGrid();
     colorBoard();
+    randomObstacle();
     drawPieces();
 })
 
@@ -104,5 +115,63 @@ function drawPieces(){
     }
 }
 
+function randomObstacle(){
+    const wallColor = 'orange';
+    const mudColor = 'brown';
+    const minRow = 5;
+    const maxRow = 11;
+    generateDirection(minRow,maxRow, wallColor);
+    generateDirection(minRow, maxRow, mudColor);
+}
+
+function generateDirection(minRow, maxRow, Color){
+    console.log("Hello")
+    //choose a random start Cell between row 4-12
+    //Math.random() creates a float between 0 and 1, startRange indicates we only want the wall to generates
+    //in the 7 rows length, minRow is our start row
+    const startRange = maxRow - minRow + 1;
+    let ranDirection;
+    let ranStart = Math.floor(Math.random() * (16 * startRange)) + 16*minRow;  
+
+    while(grid[ranStart].isObstacle == true){
+       ranStart = Math.floor(Math.random() * (16 * startRange)) + 16*minRow; 
+    }
+
+    //choose a random number of direction between 0 and 3
+    ranDirection = Math.floor(Math.random() * directions.length); 
+    grid[ranStart].show(Color);   //color the obstacle cell 
+    grid[ranStart].isObstacle = true;     //set the obstacle status of that cell
+    //create a current variable to keep track of the current Cell
+    let current = ranStart;
+
+    //loop to draw 5 more cells
+    for(let i = 0; i < 5 ; i++){  
+        //choose the next Cell
+        let chooseCell = false;  //mark that we haven't chose any cell to be our next cell
+        while (chooseCell == false){   //loop until we choose a valid next cell
+            if (ranDirection == 0 && grid[current].y > minRow && !grid[current - 16].isObstacle){    //if direction is up, not out of bound, and the cell is not an obstacle when we apply the math
+                current = current - 16;   
+                chooseCell = true;
+            }
+            else if (ranDirection == 1 && grid[current].x < 15 && !grid[current + 1].isObstacle){ //if direction is right and not in the last column
+                current = current + 1;
+                chooseCell = true;
+            }
+            else if (ranDirection == 2 && grid[current].y < (startRange + minRow) && !grid[current + 16].isObstacle){  //if direction is down and not out of bound
+                current = current + 16;
+                chooseCell = true;
+            }
+            else if (ranDirection == 3 && grid[current].x > 0 && !grid[current - 1].isObstacle){  //if direction is left and not in the 1st column
+                current = current - 1;
+                chooseCell = true;
+            }
+            else{    //if those conditions above is not fulfilled, choose another direction
+                ranDirection = Math.floor(Math.random() * directions.length);
+            }
+        }
+        grid[current].show(Color);   //color the obstacle cell 
+        grid[current].isObstacle = true;     //set the obstacle status of that cell
+    }   
+}
 
 
