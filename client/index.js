7f189645f31f9ab9b344ac5e18c09f0b6f2fddfc
xuccylicious <minhuc2006@gmail.com>
2025-03-03 21:57:46 -0500

    Updated tutorial page/Added a game guide onto index.html

--- client/index.js
+++ client/index.js
@@ -231,3 +231,31 @@ function showMoves(){
 document.getElementById("tutorialButton").addEventListener("click", () => {
     window.location.href = "tutorial.html"; // Redirect to the tutorial page
 });
+///Modal 
+ document.addEventListener('DOMContentLoaded', function() {
+    // Get the modal
+    const modal = document.getElementById("guideModal");
+    
+    // Get the button that opens the modal
+    const btn = document.getElementById("guideButton");
+    
+    // Get the <span> element that closes the modal
+    const span = document.getElementsByClassName("close-button")[0];
+    
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
